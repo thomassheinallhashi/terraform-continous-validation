fab5c7a89fbd96c712e0da70d3c71dd3d013e904
thomassheinallhashi <110841436+thomassheinallhashi@users.noreply.github.com>
2024-06-27 21:45:50 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,17 +22,3 @@ resource "aws_instance" "ubuntu" {
   }
 }
 
-check "ami_version_check" {
-  data "aws_instance" "ubuntu" {
-    instance_tags = {
-      Name = "ubuntu-instance"
-    }
-  }
-
-  assert {
-    condition = aws_instance.ubuntu.ami == data.hcp_packer_artifact.learn-packer-ubuntu.external_identifier
-    error_message = "Must use the latest available AMI, ${data.hcp_packer_artifact.hashiapp_image.external_identifier}."
-  }
-}
-
-

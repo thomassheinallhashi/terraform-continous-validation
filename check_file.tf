7ba76f17f7ce123112aa706b80dbda4804d25c42
thomassheinallhashi <110841436+thomassheinallhashi@users.noreply.github.com>
2024-06-27 23:28:46 -0400

    Update check_file.tf

--- check_file.tf
+++ check_file.tf
@@ -1,12 +1,12 @@
 check "ami_version_check" {
-  data "aws_instance" "ubuntu2" {
+  data "aws_instance" "ubuntu3" {
     instance_tags = {
-      Name = "ubuntu-234"
+      Name = "ubuntu-456"
     }
   }
 
   assert {
-    condition = aws_instance.ubuntu2.ami == data.hcp_packer_artifact.learn-packer-ubuntu.external_identifier
+    condition = aws_instance.ubuntu3.ami == data.hcp_packer_artifact.learn-packer-ubuntu.external_identifier
     error_message = "Must use the latest available AMI, ${data.hcp_packer_artifact.learn-packer-ubuntu.external_identifier}."
   }
 }

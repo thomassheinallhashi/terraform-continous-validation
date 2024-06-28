e13e43f6dd558f61cc3a4691702e315f4ff39880
thomassheinallhashi <110841436+thomassheinallhashi@users.noreply.github.com>
2024-06-27 22:13:22 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ provider "aws" {
 }
 
 provider "hcp" {
-    HCP_CLIENT_ID = zyKs4ZUF0VCIgRCgOPYnbeTbY6bbrWcZ 
-    HCP_CLIENT_SECRET = JkoCqFO4tizXPYDYXH2wdsu4YJ5Wq_kT02zPiwHtZbvGy0bBWg6niO6YjxldHe-W    
+    client_id = zyKs4ZUF0VCIgRCgOPYnbeTbY6bbrWcZ 
+    client_secret = JkoCqFO4tizXPYDYXH2wdsu4YJ5Wq_kT02zPiwHtZbvGy0bBWg6niO6YjxldHe-W    
 }
 data "hcp_packer_artifact" "learn-packer-ubuntu" {
   bucket_name   = "learn-packer-ubuntu"
